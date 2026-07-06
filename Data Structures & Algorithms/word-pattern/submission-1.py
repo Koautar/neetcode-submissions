3d32021a3bb2a2d67221a05d8a3f40283c38920c
kaoutar <134104981+Koautar@users.noreply.github.com>
2026-07-06 21:44:48 +0200

    Add: word-pattern - submission-1

---
+++ Data Structures & Algorithms/word-pattern/submission-1.py
@@ -0,0 +1,22 @@
+class Solution:
+    def wordPattern(self, pattern: str, s: str) -> bool:
+        words = s.split()
+        if len(pattern) != len(words):
+            return False  
+        char= {}
+        word = {}
+        for c,w in zip(pattern,words):
+            if c in char:
+                if char[c] != w: # if equal we do nothing mapping is already cohérent 
+                    return False 
+            else : 
+                char[c] = w
+            
+            if w in word :
+                if word[w] != c:
+                    return False 
+            else :
+                word[w] = c 
+        return True  
+
+        

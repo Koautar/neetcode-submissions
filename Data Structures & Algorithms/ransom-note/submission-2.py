56331c30dee6b4714875c63dd5f860c6eeabee32
kaoutar <134104981+Koautar@users.noreply.github.com>
2026-07-11 15:28:58 +0200

    Add: ransom-note - submission-2

---
+++ Data Structures & Algorithms/ransom-note/submission-2.py
@@ -0,0 +1,10 @@
+from collections import Counter 
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        c_magazine = Counter(magazine)
+        c_ransomNote = Counter(ransomNote)
+        constructed = True
+        for c in c_ransomNote:
+            if c_ransomNote[c] >  c_magazine[c]: 
+                constructed = False 
+        return constructed 
